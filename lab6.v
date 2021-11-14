3d6dc93c5fc5e10e5532c9613027901fcd683952
弘明 <73580589+SDFGSJ@users.noreply.github.com>
2021-11-15 00:55:47 +0800

    all works well,waited to be tested

--- lab6.v
+++ lab6.v
@@ -47,8 +47,8 @@ endmodule
 module lab06(
     input clk,
     input rst,
-    //inout PS2_CLK,
-    //inout PS2_DATA,
+    inout PS2_CLK,
+    inout PS2_DATA,
     output reg [3:0] DIGIT,
     output reg [6:0] DISPLAY,
     output reg [15:0] LED,
@@ -73,10 +73,41 @@ module lab06(
     onepulse addb1_op(.clk(clk),.pb_debounced(addbottom_debounced),.pb_1pulse(addbottom_1pulse));
     onepulse addb2_op(.clk(clk),.pb_debounced(addtop_debounced),.pb_1pulse(addtop_1pulse));
 
+    parameter [8:0] LEFT_SHIFT_CODES  = 9'b0_0001_0010;
+	parameter [8:0] RIGHT_SHIFT_CODES = 9'b0_0101_1001;
+	parameter [8:0] KEY_CODES [0:19] = {
+		9'b0_0100_0101,	// 0 => 45
+		9'b0_0001_0110,	// 1 => 16
+		9'b0_0001_1110,	// 2 => 1E
+		9'b0_0010_0110,	// 3 => 26
+		9'b0_0010_0101,	// 4 => 25
+		9'b0_0010_1110,	// 5 => 2E
+		9'b0_0011_0110,	// 6 => 36
+		9'b0_0011_1101,	// 7 => 3D
+		9'b0_0011_1110,	// 8 => 3E
+		9'b0_0100_0110,	// 9 => 46
+		
+		9'b0_0111_0000, // right_0 => 70
+		9'b0_0110_1001, // right_1 => 69
+		9'b0_0111_0010, // right_2 => 72
+		9'b0_0111_1010, // right_3 => 7A
+		9'b0_0110_1011, // right_4 => 6B
+		9'b0_0111_0011, // right_5 => 73
+		9'b0_0111_0100, // right_6 => 74
+		9'b0_0110_1100, // right_7 => 6C
+		9'b0_0111_0101, // right_8 => 75
+		9'b0_0111_1101  // right_9 => 7D
+	};
 
-    /*wire [511:0] key_down;
+	wire [511:0] key_down;
     wire [8:0] last_change;
     wire key_valid;
+	reg [3:0] key_num;
+    
+	wire shift_down;
+	assign shift_down = (key_down[LEFT_SHIFT_CODES] == 1'b1 || key_down[RIGHT_SHIFT_CODES] == 1'b1) ? 1'b1 : 1'b0;
+
+    
     KeyboardDecoder keydecode(
         .key_down(key_down),
         .last_change(last_change),
@@ -85,7 +116,7 @@ module lab06(
         .PS2_CLK(PS2_CLK),
         .rst(rst),
         .clk(clk)
-    );*/
+    );
 
 
     integer i;
@@ -162,10 +193,34 @@ module lab06(
         get_revenue_finish_next = get_revenue_finish;
         addfuel_finish_next = addfuel_finish;
 
-
+        if(key_valid && key_down[last_change]) begin
+            if(key_num!=4'b1111) begin
+                if(!shift_down) begin
+                    if(key_num==4'b0001) begin
+                        if(LED[15:14]==2'b00) begin
+                            led_next[15:14]=2'b10;
+                        end else if(LED[15:14]==2'b10) begin
+                            led_next[15:14]=2'b11;
+                        end else begin
+                            led_next[15:14]=2'b11;
+                        end
+                    end else if(key_num==4'b0010) begin
+                        if(LED[12:11]==2'b00) begin
+                            led_next[12:11]=2'b10;
+                        end else if(LED[12:11]==2'b10) begin
+                            led_next[12:11]=2'b11;
+                        end else begin
+                            led_next[12:11]=2'b11;
+                        end
+                    end
+                end/* else begin
+                    //dont know what to write
+                end*/
+            end
+        end
 
         //can control led at any state,detect keyboard input
-        if(addbottom_1pulse) begin
+        /*if(addbottom_1pulse) begin
             if(LED[15:14]==2'b00) begin
                 led_next[15:14]=2'b10;
             end else if(LED[15:14]==2'b10) begin
@@ -181,7 +236,7 @@ module lab06(
             end else begin
                 led_next[12:11]=2'b11;
             end
-        end
+        end*/
 
         if(work_clk_1p) begin
             if(state==BOTTOM) begin
@@ -435,6 +490,32 @@ module lab06(
         end
     end
 
+
+    always @ (*) begin
+		case (last_change)
+			KEY_CODES[00] : key_num = 4'b0000;
+			KEY_CODES[01] : key_num = 4'b0001;
+			KEY_CODES[02] : key_num = 4'b0010;
+			KEY_CODES[03] : key_num = 4'b0011;
+			KEY_CODES[04] : key_num = 4'b0100;
+			KEY_CODES[05] : key_num = 4'b0101;
+			KEY_CODES[06] : key_num = 4'b0110;
+			KEY_CODES[07] : key_num = 4'b0111;
+			KEY_CODES[08] : key_num = 4'b1000;
+			KEY_CODES[09] : key_num = 4'b1001;
+			KEY_CODES[10] : key_num = 4'b0000;
+			KEY_CODES[11] : key_num = 4'b0001;
+			KEY_CODES[12] : key_num = 4'b0010;
+			KEY_CODES[13] : key_num = 4'b0011;
+			KEY_CODES[14] : key_num = 4'b0100;
+			KEY_CODES[15] : key_num = 4'b0101;
+			KEY_CODES[16] : key_num = 4'b0110;
+			KEY_CODES[17] : key_num = 4'b0111;
+			KEY_CODES[18] : key_num = 4'b1000;
+			KEY_CODES[19] : key_num = 4'b1001;
+			default		  : key_num = 4'b1111;
+		endcase
+	end
     //7-segment control
     always @(posedge display_clk) begin
         case(DIGIT)
